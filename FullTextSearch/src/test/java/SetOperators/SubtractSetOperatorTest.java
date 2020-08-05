6102eab4ea10094ffc03e91c109bd32d2daeebd8
titansarus <amirm137878@gmail.com>
2020-08-05 16:21:43 +0430

    SetOperators Full Functionality Test

--- FullTextSearch/src/test/java/SetOperators/SubtractSetOperatorTest.java
+++ FullTextSearch/src/test/java/SetOperators/SubtractSetOperatorTest.java
@@ -1,14 +1,28 @@
 package SetOperators;
 
+import Model.InvertedIndex;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class SubtractSetOperatorTest {
+    private static Map<String, Set<String>> indexMap;
+
+    @BeforeAll
+    public static void initialize() {
+        indexMap = new HashMap<>();
+        Set<String> helloIndex = new HashSet<>(Arrays.asList("1", "2", "3", "4"));
+        Set<String> worldIndex = new HashSet<>(Arrays.asList("2", "5"));
+        Set<String> hiIndex = new HashSet<>(Arrays.asList("2", "4"));
+        indexMap.put("hello", helloIndex);
+        indexMap.put("world", worldIndex);
+        indexMap.put("hi", hiIndex);
+    }
 
     @Test
     public void notEmptyInitialSetCommonElementTest() {
@@ -40,6 +54,19 @@ class SubtractSetOperatorTest {
         Set<String> subMask = new HashSet<>(Arrays.asList("z", "y"));
         new SubtractSetOperator().specificOperation(result, subMask);
         org.assertj.core.api.Assertions.assertThat(result).doesNotContainSequence("z", "y");
+    }
+
+    @Test
+    public void testFullFunctionalityOfSubtractOnInvertedIndex() {
+        InvertedIndex invertedIndex = mock(InvertedIndex.class);
+
+        when(invertedIndex.getIndices()).thenReturn(indexMap);
+
+        Set<String> initial = indexMap.get("hello");
+        Set<String> result= new SubtractSetOperator().operate(initial , Arrays.asList("hi" , "world") , invertedIndex);
+        org.assertj.core.api.Assertions.assertThat(result).containsOnly("1","3");
+
+
     }
 
 }
