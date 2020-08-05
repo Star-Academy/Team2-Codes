50e88ff29a18d405f53e9f4af739c06eba3e3b34
titansarus <amirm137878@gmail.com>
2020-08-05 14:25:56 +0430

    Maven is Okay

---
+++ FullTextSearch/src/test/java/SetOperators/AndSetOperatorTest.java
@@ -0,0 +1,25 @@
+package SetOperators;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AndSetOperatorTest {
+    @Test
+    public void testAndWorksFine() {
+        Set<String> result = new HashSet<>();
+        Set<String> andMask = new HashSet<>();
+        result.addAll(Arrays.asList("a", "b", "c"));
+        andMask.addAll(Arrays.asList("a", "b"));
+        new AndSetOperator().specificOperation(result, andMask);
+        org.assertj.core.api.Assertions.assertThat(result).containsOnly("a", "b");
+    }
+
+}
