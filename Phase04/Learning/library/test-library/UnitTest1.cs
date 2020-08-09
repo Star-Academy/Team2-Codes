8871fa79589ef588b9244d84005197b62191a70a
titansarus <amirm137878@gmail.com>
2020-08-09 14:24:52 +0430

    Test Pass

--- Phase04/Learning/library/test-library/UnitTest1.cs
+++ Phase04/Learning/library/test-library/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace TestApp
         [Fact]
         public void TestThing()
         {
-            Assert.NotEqual(42, new Thing().Get(19, 23));
+            Assert.Equal(42, new Thing().Get(19, 23));
         }
     }
 }
