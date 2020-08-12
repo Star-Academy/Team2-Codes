1f7e5a18871abada362aab92c8b680b43e45e828
titansarus <amirm137878@gmail.com>
2020-08-12 12:08:11 +0430

    Some Test

---
+++ FullTextSearchCsharp/FullTextSearch/FullTextSearch.Test/Utility/TokenizerTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using FullTextSearch.Utility;
+using NFluent;
+using Xunit;
+
+namespace FullTextSearch.Test.Utility
+{
+    public class TokenizerTest
+    {
+        [Fact]
+        public void TokenizeContentTest()
+        {
+            string text = "hello world, new, amir. javad (Hi)";
+            var tokenizer = new Tokenizer();
+            var tokenized = tokenizer.TokenizeContent(text);
+            Check.That(tokenized).ContainsExactly("hello", "world", "new", "amir", "javad", "Hi");
+        }
+    }
+}
---
+++ FullTextSearchCsharp/FullTextSearch/FullTextSearch/Utility/Tokenizer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace FullTextSearch.Utility
+{
+    public class Tokenizer
+    {
+        // " ,.;-'()\"@[]><\t\n";
+
+        private readonly char[] separators = new char[]
+            {' ', ',', '.', ';', '-', '\'', '(', ')', '\"', '@', '[', ']', '>', '<', '\t', '\n' , '\0'};
+
+        public ISet<string> TokenizeContent(string content)
+        {
+            var splitted = content.Split(separators, StringSplitOptions.RemoveEmptyEntries).ToList();
+            splitted.Remove("");
+
+            return new HashSet<string>(splitted);
+        }
+    }
+}
