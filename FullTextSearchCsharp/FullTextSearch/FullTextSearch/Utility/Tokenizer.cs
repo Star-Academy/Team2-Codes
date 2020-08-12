ee2ad6d83fae00af6b10b990c0d8ceff41f822e2
MJavad Hezareh <55049080+MJavadHzr@users.noreply.github.com>
2020-08-12 12:26:28 +0430

    tokenizer component

--- FullTextSearchCsharp/FullTextSearch/FullTextSearch/Utility/Tokenizer.cs
+++ FullTextSearchCsharp/FullTextSearch/FullTextSearch/Utility/Tokenizer.cs
@@ -1,23 +1,24 @@
 ﻿using System.Collections.Generic;
-using System.Dynamic;
 using System.Linq;
-using System.Text.RegularExpressions;
+using System;
+using FullTextSearch.Model;
 
 namespace FullTextSearch.Utility
 {
     public class Tokenizer
     {
-        // " ,.;-'()\"@[]><\t\n";
-
         private readonly char[] separators = new char[]
             {' ', ',', '.', ';', '-', '\'', '(', ')', '\"', '@', '[', ']', '>', '<', '\t', '\n' , '\0'};
 
         public ISet<string> TokenizeContent(string content)
         {
             var splitted = content.Split(separators, StringSplitOptions.RemoveEmptyEntries).ToList();
-            splitted.Remove("");
-
             return new HashSet<string>(splitted);
+        }
+
+        public void TokenizeAllDocuments(List<Document> allDocuments)
+        {
+            allDocuments.ForEach(d => d.TokenizedWords = TokenizeContent(d.Content));
         }
     }
 }
