39c191fc0f4782d7195c7dcafc513484932ecb32
titansarus <amirm137878@gmail.com>
2020-08-16 17:01:31 +0430

    Phase 06 Console

---
+++ FullTextSearchCsharp/FullTextSearch/FullTextSearch.Console/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using FullTextSearch.QueryProcessors;
+using FullTextSearch.Utility.Readers;
+
+namespace FullTextSearch.Console
+{
+    class Program
+    {
+        private const string address = "..\\..\\..\\..\\resources\\EnglishData";
+
+        static void Main(string[] args)
+        {
+            FileReader fileReader = new FileReader(address);
+            var documents = fileReader.GetDocuments();
+            QueryProcessor queryProcessor = new QueryProcessor(documents);
+
+            while (true)
+            {
+                string query = System.Console.ReadLine();
+                var answerIds = queryProcessor.PerformSearch(query);
+                PrintFormatted(answerIds);
+            }
+        }
+
+        static void PrintFormatted(IEnumerable<string> idList)
+        {
+            foreach (var id in idList)
+            {
+                System.Console.Write($"{id} ");
+            }
+
+            System.Console.WriteLine();
+        }
+    }
+}
