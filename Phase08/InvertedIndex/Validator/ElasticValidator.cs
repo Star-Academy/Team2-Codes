668f0e4b656ba01b34e6ab15b395c370fb8e1833
titansarus <amirm137878@gmail.com>
2020-08-26 12:44:35 +0430

    Refactor Long Method

--- Phase08/InvertedIndex/Validator/ElasticValidator.cs
+++ Phase08/InvertedIndex/Validator/ElasticValidator.cs
@@ -9,32 +9,54 @@ namespace Validator
         {
             if (response.ApiCall.Success && response.IsValid)
             {
-                return new ValidationResult
-                    {IsValid = true, ElasticException = null, HttpStatusCode = response.ApiCall.HttpStatusCode};
+                return NoErroResult(response);
             }
 
             if (response.ServerError != null)
             {
-                return new ValidationResult
-                {
-                    IsValid = false, ElasticException = new ElasticServerException(response.ServerError.ToString()),
-                    HttpStatusCode = response.ApiCall.HttpStatusCode
-                };
+                return ServerErrorResult(response);
             }
 
             if (response.ApiCall.OriginalException != null)
             {
-                return new ValidationResult
-                {
-                    IsValid = false,
-                    ElasticException = new ElasticClientException(response.ApiCall.OriginalException.Message),
-                    HttpStatusCode = response.ApiCall.HttpStatusCode
-                };
+                return ClientErrorResult(response);
             }
 
+            return UnknownErrorResult(response);
+        }
+
+        public static ValidationResult NoErroResult(IResponse response)
+        {
+            return new ValidationResult
+                {IsValid = true, ElasticException = null, HttpStatusCode = response.ApiCall.HttpStatusCode};
+        }
+
+        public static ValidationResult ServerErrorResult(IResponse response)
+        {
             return new ValidationResult
             {
-                IsValid = false, ElasticException = new ElasticClientException(),
+                IsValid = false,
+                ElasticException = new ElasticServerException(response.ServerError.ToString()),
+                HttpStatusCode = response.ApiCall.HttpStatusCode
+            };
+        }
+
+        public static ValidationResult ClientErrorResult(IResponse response)
+        {
+            return new ValidationResult
+            {
+                IsValid = false,
+                ElasticException = new ElasticServerException(response.ServerError.ToString()),
+                HttpStatusCode = response.ApiCall.HttpStatusCode
+            };
+        }
+
+        public static ValidationResult UnknownErrorResult(IResponse response)
+        {
+            return new ValidationResult
+            {
+                IsValid = false,
+                ElasticException = new ElasticClientException(),
                 HttpStatusCode = response.ApiCall.HttpStatusCode
             };
         }
