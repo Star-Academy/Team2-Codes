ca833b7b636a61982b107f21c47969d8d8754b7c
titansarus <amirm137878@gmail.com>
2020-09-01 12:33:41 +0430

    removed unused usings

--- Phase09/Backend/InvertedIndexApi/InvertedIndexApi/Controllers/InvertedIndexController.cs
+++ Phase09/Backend/InvertedIndexApi/InvertedIndexApi/Controllers/InvertedIndexController.cs
@@ -1,14 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Threading.Tasks;
-using Elasticsearch.Net;
 using InvertedIndex.QueryProcessor;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Validator.Exceptions;
 
 namespace InvertedIndexApi.Controllers
 {
